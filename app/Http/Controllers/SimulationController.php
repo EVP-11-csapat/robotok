2e6c146d082a85a79d130da563e15d39398ed9f8
Riszterer István <gwlxts@gmail.com>
2023-05-10 13:44:22 +0200

    TotalCost incrementation and decrementation added

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SimulationController extends Controller
 {
-    //
+    public function incrementTotalCost(Request $request): JsonResponse
+    {
+        $cost = $request->all()['value'];
+        DB::table('simulation')->where('id', 1)->increment('totalCost',$cost);
+        DB::table('simulation')->save();        
+        return response()->json(['success' => $request->all()['cost']]);
+    }
+    // A függvény a robot árának felét várja paraméterként,
+    // (automatikusan nem fogja megfelezni a metódus requestben kapott cost-ot)
+    public function decrementTotalCost(Request $request): JsonResponse
+    {
+        $cost = $request->all()['value'];
+        DB::table('simulation')->where('id', 1)->decrement('totalCost',$cost);
+        DB::table('simulation')->save();        
+        return response()->json(['success' => $request->all()['cost']]);
+    }
 }
