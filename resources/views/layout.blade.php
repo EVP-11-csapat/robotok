2c98878eec942dc5181ccd336777eb13cc74b4a8
Nemeth-Tamas <tamas.nemeth2002@gmail.com>
2023-05-10 10:19:09 +0200

    Added footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -45,9 +45,27 @@
         </div>
     </nav>
 
-    <div class="container mx-auto px-4 min-h-full">
+    <div class="container mx-auto px-4 min-h-screen">
         @yield('content')
     </div>
+
+
+    <footer class="bg-white rounded-lg shadow m-4">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="https://robot.roh.boats/"
+                    class="hover:underline">11. Csapat™</a>. All Rights Reserved.
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+                <li>
+                    <a href="https://github.com/EVP-11-csapat" class="mr-4 hover:underline md:mr-6">Github</a>
+                </li>
+                <li>
+                    <a href="/contact" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+
 </body>
 
 </html>
