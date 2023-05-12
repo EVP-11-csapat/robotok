5dfeca7be732b98772d4fc071b783cad1c1d9c4f
excf28 <deee4444@gmail.com>
2023-05-12 13:56:57 +0200

    comments for api routes

--- routes/api.php
+++ routes/api.php
@@ -22,23 +22,64 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// /addrobot - add a new robot - data: {id: "storeID"}
+/* /addrobot - add a new robot
+ *  data:
+ *     "id" - id of store template for the robot
+ */
 Route::post("/addrobot", [RobotController::class, 'addRobot']);
-// /addcharger - add a new charger - data: {id: "storeID"}
+
+/* /addcharger - add a new charger
+ *  data:
+ *     "id" - id of store template for the charger
+ */
 Route::post("/addcharger", [ChargerController::class, 'addCharger']);
 
-// /activaterobot - activate a robot - data: {id: "robotID",  active: true/false}
+/* /activaterobot - set active status of a robot
+ *  data:
+ *     "id" - id of the robot to activate
+ *     "active" - true/false to activate/deactivate the robot
+ */
 Route::post("/activaterobot", [RobotController::class, 'activateRobot']);
-// /activatecharger - activate a charger - data: {id: "chargerID",  active: true/false}
+/* /activatecharger - set active status of a charger
+ *  data:
+ *     "id" - id of the charger to activate
+ *     "active" - true/false to activate/deactivate the charger
+ */
 Route::post("/activatecharger", [ChargerController::class, 'activateCharger']);
 
-// /getrobots - get all robots - returns: [{id: "robotID",  charge: "charge", active: true/false, active_hours:  "activeHours", model: "model"}]
+/*  /getrobots - get all robots
+ *  returns:
+ *    [
+ *    "id": "robotID",
+ *    "charge": "charge",
+ *    "active": "active",
+ *    "active_hours": "activeHours",
+ *    "model": "model"
+ *    ]
+ */
 Route::get("/getrobots", [RobotController::class, 'getRobots']);
-// /getchargers - get all chargers - returns: [{id: "chargerID",  chargee: "robotID", active: true/false, active_hours:  "activeHours", model: "model"}]
+/*  /getchargers - get all chargers
+ *  returns:
+ *    [
+ *    "id": "chargerID",
+ *    "chargee": "robotID",
+ *    "active": "active",
+ *    "active_hours": "activeHours",
+ *    "model": "model"
+ *    ]
+ */
 Route::get("/getchargers", [ChargerController::class, 'getChargers']);
 
-// /importcargo - generate cargo - data: {amount: "amount",  day: "day"}
+/* /importcargo - generate cargo
+*  data :
+*    "amount" - amount of cargo to generate
+*    "day" - arrival day of the generated cargo
+*/
 Route::post("/importcargo", [GeneratedCargoController::class, 'importCargo']);
 
-// /simulate -  simulate a day
+/*  /simulate -  simulate a day
+*  returns :
+*    "remainingCargo" - cargo left in the warehouse at the  end of the day
+*    "log" - log of events during the day
+*/
 Route::get("/simulate", [SimulationController::class, 'simulate']);
